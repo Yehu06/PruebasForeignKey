9319d777fa68e7baca8246b0852eb261fe8150a2
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2026-01-23 11:05:10 +0100

    Update SQL-INSERT-COLEGIOS.sql

--- SQL-INSERT-COLEGIOS.sql
+++ SQL-INSERT-COLEGIOS.sql
@@ -2,8 +2,8 @@ USE Potencial_Digital_SQL2;
 
 
 
-INSERT INTO colegios (idColegio, nombre) VALUES (0, 'plazas disponibles'); 
-INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto) VALUES (1, 'IES Central', 'Juan Pérez', '600123456');
+INSERT INTO colegios (idColegio, nombre) VALUES (0, 'plazas disponibles');--Insertamos la primeras fila con los valores por defecto de telefono y nombre de contacto
+INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto) VALUES (1, 'IES Central', 'Juan PÃ©rez', '600123456');
 INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto) VALUES (2, 'IES WEST', 'Manolo', '605623426');
 INSERT INTO colegios_actividades (idColegio, idCharla, fecha, numero_asistentes) VALUES (1, 3, '2026-02-01', 30);
 INSERT INTO colegios_actividades (idCharla, fecha, numero_asistentes) VALUES (3, '2026-02-05', 20); --Para probar el valor por defecto de colegio
