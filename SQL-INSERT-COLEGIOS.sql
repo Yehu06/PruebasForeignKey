d1239cc332d644d1b27ac9893eafa410a2529245
Yehu06 <yehublancopinilla.guadalupe@alumnado.fundacionloyola.net>
2026-01-23 11:03:29 +0100

    Add files via upload

---
+++ SQL-INSERT-COLEGIOS.sql
@@ -0,0 +1,10 @@
+USE Potencial_Digital_SQL2; 
+
+
+
+INSERT INTO colegios (idColegio, nombre) VALUES (0, 'plazas disponibles'); 
+INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto) VALUES (1, 'IES Central', 'Juan Pérez', '600123456');
+INSERT INTO colegios (idColegio, nombre, persona_contacto, tfno_contacto) VALUES (2, 'IES WEST', 'Manolo', '605623426');
+INSERT INTO colegios_actividades (idColegio, idCharla, fecha, numero_asistentes) VALUES (1, 3, '2026-02-01', 30);
+INSERT INTO colegios_actividades (idCharla, fecha, numero_asistentes) VALUES (3, '2026-02-05', 20); --Para probar el valor por defecto de colegio
+INSERT INTO colegios_actividades (idColegio, idCharla, fecha, numero_asistentes)VALUES (2, NULL, '2026-02-10', 15); --Para probar el id char a null
